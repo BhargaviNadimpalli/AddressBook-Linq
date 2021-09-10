d563911c902b730fa663ec393a3791d99df4185a
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-09-10 17:45:52 -0600

    Feat: Created data in address book

--- AddressBookLinq/AddressBook.cs
+++ AddressBookLinq/AddressBook.cs
@@ -8,5 +8,17 @@ namespace AddressBookLinq
     public class AddressBook
     {
         DataTable table = new DataTable("AddressBook");
+
+        public void AddressBookData()
+        {
+            table.Columns.Add("FirstName", typeof(string));
+            table.Columns.Add("LastName", typeof(string));
+            table.Columns.Add("Address", typeof(string));
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("State", typeof(string));
+            table.Columns.Add("Zip", typeof(string));
+            table.Columns.Add("PhoneNumber", typeof(string));
+            table.Columns.Add("Email", typeof(string));
+        }
     }
 }
